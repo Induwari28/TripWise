f8bec14c004a88251e693fcdb921215b8e84fe12
induwari senewirathne <upamalisenewirathne@gmail.com>
2026-09-10 17:27:02 +0530

    Added vercel to allowed cors

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -141,6 +141,7 @@ STATIC_URL = 'static/'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://trip-wise-gules.vercel.app",
 ]
 
 # REST Framework Configuration
